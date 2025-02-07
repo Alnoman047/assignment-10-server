096bb20578ef0fc95db2f523be085f44c97f9785
captainboogey <alnoman047@gmail.com>
2025-02-08 04:44:14 +0600

    single data load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -40,6 +40,12 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result)
 
+    })
+    app.get("/addItems/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await itemsCollection.findOne(query);
+        res.send(result);
     })
 
 
