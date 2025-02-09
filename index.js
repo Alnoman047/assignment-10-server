495e285c621e502f7049ca1d3e4f5cf737dfd11a
captainboogey <alnoman047@gmail.com>
2025-02-10 03:09:21 +0600

    sigle dataset on card

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/cards/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cardCollection.findOne(query);
+      res.send(result)
+    })
+
 
 
 
